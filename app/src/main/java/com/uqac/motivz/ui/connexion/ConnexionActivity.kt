2be4599bdae39f72178bc4615ba91ccd96e7aee5
Camille Benoit <camille.benoit@mines-ales.org>
2021-11-05 15:03:17 -0400

    fragment container done

--- app/src/main/java/com/uqac/motivz/ui/connexion/ConnexionActivity.kt
+++ app/src/main/java/com/uqac/motivz/ui/connexion/ConnexionActivity.kt
@@ -1,10 +1,39 @@
 package com.uqac.motivz.ui.connexion
 
+import android.app.Activity;
+import com.uqac.motivz.databinding.ActivityConnexionBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.uqac.motivz.R
+import com.uqac.motivz.databinding.FragmentAvatarShopBinding
 
+class ConnexionActivity : AppCompatActivity() {
 
-class ConnexionActivity  {
+    lateinit var binding : ActivityConnexionBinding
 
-    lateinit var binding: ConnexionActivityBinding
+    override fun onCreate (savedInstanceState : Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = ActivityConnexionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logInBtn.setOnClickListener(){
+            replaceFragment(LogInFragment())
+        }
+
+        binding.signInBtn.setOnClickListener(){
+            replaceFragment(SignInFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val framgmentManager = supportFragmentManager
+        val fragmentTransaction = framgmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.connexionFragContainer, fragment)
+        fragmentTransaction.commit()
+    }
 
 
 }
