6f1375c503eef87f3abeb090a814d45cf9f6705e
Amanda RETORY <retorya@3il.fr>
2021-11-08 17:55:23 -0500

    Display goals in a list view

--- app/src/main/java/com/uqac/motivz/ui/home/HomeFragment.kt
+++ app/src/main/java/com/uqac/motivz/ui/home/HomeFragment.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.marginBottom
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.uqac.motivz.R
 import com.uqac.motivz.databinding.FragmentHomeBinding
+import android.app.ProgressDialog
+
+
+
 
 class HomeFragment : Fragment() {
 
@@ -31,10 +39,25 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        //val textView: TextView = binding.textHome
+        //homeViewModel.text.observe(viewLifecycleOwner, Observer {
+          //  textView.text = it
+        //})
+
+        val goalTitle: TextView = binding.goalTitle
+        goalTitle.text = getString(R.string.goal_title)
+
+        val goalLinearLayout: LinearLayout = binding.goalLinearLayout
+
+        val goalList = listOf("Objectif 1", "Objectif 2", "Objectif 3", "Objectif 4", "Objectif 5",
+            "Objectif 6", "Objectif 7", "Objectif 8", "Objectif 9", "Objectif 10")
+        for (goal in goalList) {
+            var button = Button(this.context)
+            button.text = goal
+            button.height = 200
+            goalLinearLayout.addView(button);
+        }
+
         return root
     }
 
