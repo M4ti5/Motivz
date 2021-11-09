43672c57752cf4d9114ca05ace790f6c8f1f4b4a
Camille Benoit <camille.benoit@mines-ales.org>
2021-11-09 16:38:33 -0500

    final udpate

--- app/src/main/java/com/uqac/motivz/ui/profil/ProfilActivity.kt
+++ app/src/main/java/com/uqac/motivz/ui/profil/ProfilActivity.kt
@@ -1,10 +1,14 @@
 package com.uqac.motivz.ui.profil
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.uqac.motivz.MainActivity
 import com.uqac.motivz.R
 import com.uqac.motivz.databinding.ActivityProfilBinding
+import com.uqac.motivz.ui.connexion.SignInFragment
 
 class ProfilActivity  : AppCompatActivity() {
 
@@ -19,6 +23,17 @@ class ProfilActivity  : AppCompatActivity() {
 
         pseudo = intent.getStringExtra("PSEUDONYME").toString()
 
+        binding.pseudoProfil.text = pseudo
 
+        binding.goBackBtn.setOnClickListener(){
+            goToMainActivity()
+        }
+
+    }
+
+    private fun goToMainActivity(){
+        val intent = Intent(this,  MainActivity::class.java)
+        intent.putExtra("PSEUDONYME", pseudo);
+        startActivity(intent)
     }
 }
