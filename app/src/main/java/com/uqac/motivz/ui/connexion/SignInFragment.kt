9611868b9a2f3ab401add9ac37d4fbd4088969df
Amanda RETORY <retorya@3il.fr>
2021-12-08 09:45:05 -0500

    fix connection

--- app/src/main/java/com/uqac/motivz/ui/connexion/SignInFragment.kt
+++ app/src/main/java/com/uqac/motivz/ui/connexion/SignInFragment.kt
@@ -34,7 +34,7 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
         val btn = view.findViewById<Button>(R.id.btnSignIn)
 
         btn.setOnClickListener{
-            val pseudo = view.findViewById<EditText>(R.id.enterPseudoSI).getText().toString()
+            val pseudo = view.findViewById<EditText>(R.id.enterPseudoSI).text.toString()
             val mail = view.findViewById<EditText>(R.id.enterEmailSI).text.toString()
             val password = view.findViewById<EditText>(R.id.enterPasswordSI).text.toString()
 
@@ -43,12 +43,12 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
                     val database = Firebase.database
                     val myRef = database.getReference("users").child(auth.uid.toString()).child("pseudo")
                     myRef.setValue(pseudo)
+                    goToMainActivity(pseudo, R.id.navigation_home)
                 }
             }.addOnFailureListener { exception ->
                 //val context = context as MainActivity
-                //Toast.makeText(context,exception.localizedMessage,Toast.LENGTH_LONG).show()
+                Toast.makeText(this.context,exception.localizedMessage,Toast.LENGTH_LONG).show()
             }
-            goToMainActivity(pseudo, R.id.navigation_home)
         }
         // Inflate the layout for this fragment
         return view
