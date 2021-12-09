20139c90371494fc1b46813e1c609f987e08875e
Camille Benoit <camille.benoit@mines-ales.org>
2021-12-09 03:56:11 -0500

    items

--- app/src/main/java/com/uqac/motivz/ui/shop/ItemShopFragment.kt
+++ app/src/main/java/com/uqac/motivz/ui/shop/ItemShopFragment.kt
@@ -1,17 +1,22 @@
 package com.uqac.motivz.ui.shop
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.uqac.motivz.databinding.FragmentItemListBinding
 
 import android.widget.ListView
+import android.widget.Toast
 import com.uqac.motivz.Adapters.ItemListAdapter
 import com.uqac.motivz.MainActivity
 import com.uqac.motivz.Model.Item
+import com.uqac.motivz.R
 
 
 class ItemShopFragment : Fragment() {
@@ -38,23 +43,56 @@ class ItemShopFragment : Fragment() {
 
     override fun onStart()  {
         super.onStart()
-        items.add(Item("couleur de cheveux", 300))
-        items.add(Item("bague", 200))
-        items.add(Item("chaussures", 100))
-        items.add(Item("coupe de cheveux", 500))
-        items.add(Item("pantalon", 150))
+        items.add(Item("Freeze", 300))
+        items.add(Item("Double score", 200))
 
         listView = binding.itemList
         val context = context as MainActivity
         val adapter = ItemListAdapter(context, items)
         listView?.adapter = adapter
 
-
+        listView!!.setOnItemClickListener { parent, view, pos, id ->
+            askToBuyItem(parent, pos)
+        }
     }
 
     override fun onStop() {
         super.onStop()
         // Made save of avatar
+    }
+
+
+    fun askToBuyItem(parent : AdapterView<*>, pos : Int){
+        val selectedItem = parent.getItemAtPosition(pos) as Item
+
+        val alertDialog: AlertDialog? = activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.apply {
+                setPositiveButton(
+                    R.string.yes,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User clicked OK button
+                        buyItem(selectedItem)
+                    })
+                setNegativeButton(R.string.no,
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            }
+
+            builder?.setMessage("Acheter " + selectedItem.name + " ?")
+
+            // Create the AlertDialog
+            builder.create()
+        }
+
+        alertDialog?.show()
+
+
+    }
+
+    fun buyItem(item : Item){
+        Toast.makeText(context, item.name, Toast.LENGTH_SHORT).show()
     }
 
 }
