35b9948b7d413057f498898e6439511413a4f2db
M4ti5 <Caffiauxmatis@gmail.com>
2021-12-09 02:53:53 +0100

    coins

--- app/src/main/java/com/uqac/motivz/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/uqac/motivz/ui/shop/ShopFragment.kt
@@ -5,10 +5,18 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.uqac.motivz.MainActivity
 import com.uqac.motivz.R
 import com.uqac.motivz.databinding.ActivityConnexionBinding
 
@@ -20,32 +28,18 @@ class ShopFragment : Fragment() {
 
     private lateinit var shopViewModel: ShopViewModel
     private var _binding: FragmentShopBinding? = null
-
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
-        shopViewModel = ViewModelProvider(this).get(ShopViewModel::class.java)
-        _binding = FragmentShopBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        insertNestedFragment()
-    }
+    private lateinit var database : FirebaseDatabase
+    private lateinit var auth : FirebaseAuth
 
-    override fun onStart() {
-        super.onStart()
-        replaceFragment(AvatarShopFragment())
+    private lateinit var user : FirebaseUser
+    private lateinit var uid : String
+    private lateinit var pseudo: String
 
-        binding.avatarBtn.setOnClickListener(){
-            replaceFragment(AvatarShopFragment())
-        }
 
-        binding.itemsBtn.setOnClickListener(){
-            replaceFragment(ItemShopFragment())
-        }
-    }
+
     private fun insertNestedFragment() {
         val childFragment: Fragment = AvatarShopFragment()
         val transaction: FragmentTransaction = childFragmentManager.beginTransaction()
@@ -58,4 +52,48 @@ class ShopFragment : Fragment() {
         fragmentTransaction.replace(R.id.connexionShopFrag, fragment)
         fragmentTransaction.commit()
     }
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
+        shopViewModel = ViewModelProvider(this).get(ShopViewModel::class.java)
+        _binding = FragmentShopBinding.inflate(inflater, container, false)
+
+
+        database = Firebase.database
+        val auth = FirebaseAuth.getInstance()
+        if(auth.currentUser != null){
+            user = auth.currentUser!!
+            uid = user.uid
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        insertNestedFragment()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        ////Fragment Management
+        replaceFragment(AvatarShopFragment())
+
+        binding.avatarBtn.setOnClickListener(){
+            replaceFragment(AvatarShopFragment())
+        }
+
+        binding.itemsBtn.setOnClickListener(){
+            replaceFragment(ItemShopFragment())
+        }
+
+        ////Coin Management
+        database.reference.child("users").child(uid).child("coins").get().addOnSuccessListener {
+            view?.findViewById<TextView>(R.id.nb_coins)?.text = it.value.toString()
+        }
+
+
+    }
+
+
 }
